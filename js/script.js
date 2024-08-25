8c379539a09bcd411f890a1e4bacec991d9c9a58
Ivan <mauzer.bruno@yandex.ru>
2024-08-26 00:39:49 +0300

    Удалена яндекс.карта, добавлена карта от OpenLayers v.4.6.5

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+
+
+// var map = new ol.Map({
+//     target: 'map',
+//     layers: [
+//       new ol.layer.Tile({
+//         source: new ol.source.OSM()
+//       })
+//     ],
+//     view: new ol.View({
+//       projection: 'EPSG:4326',
+//       center: ol.proj.toLonLat([40.271546, 48.316393], 'EPSG:4326'),
+//       zoom: 20,
+//     })
+    
+//   });
+
+//   console.log('hiiii')
